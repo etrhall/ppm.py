b27867c4af6a10d53ed02388f8899b9a71341d55
etrhall <etrhall@gmail.com>
2026-04-08 15:54:06 +0200

    add type asserts

--- src/ppm.py
+++ src/ppm.py
@@ -24,6 +24,17 @@ class PPMSimple(_ppm.ppm_simple):
             )
         self.alphabet_levels = alphabet_levels
 
+        assert isinstance(alphabet_size, int), "alphabet_size must be int (or None)"
+        assert isinstance(order_bound, int), "order_bound must be int"
+        assert isinstance(shortest_deterministic, bool), (
+            "shortest_deterministic must be bool"
+        )
+        assert isinstance(exclusion, bool), "exclusion must be bool"
+        assert isinstance(update_exclusion, bool), "update_exclusion must be bool"
+        escape_methods = ["a", "b", "c", "d", "ax"]
+        if escape not in escape_methods:
+            raise Exception(f"escape parameter must be one of {escape_methods}")
+
         super().__init__(
             alphabet_size,
             order_bound,
@@ -32,6 +43,7 @@ class PPMSimple(_ppm.ppm_simple):
             update_exclusion,
             escape,
         )
+        self.debug_smooth = debug_smooth
 
     def model_seq(
         self,
@@ -51,6 +63,12 @@ class PPMSimple(_ppm.ppm_simple):
                 )
             self.alphabet_levels = self.alphabet_levels + list(alphabet_diff)
         iseq = [self.alphabet_levels.index(x) for x in seq]
+
+        assert isinstance(train, bool), "train must be bool"
+        assert isinstance(predict, bool), "predict must be bool"
+        assert isinstance(return_distribution, bool), "return_distribution must be bool"
+        assert isinstance(return_entropy, bool), "return_entropy must be bool"
+        assert isinstance(generate, bool), "generate must be bool"
 
         result = super().model_seq(
             iseq, time, train, predict, return_distribution, return_entropy, generate
